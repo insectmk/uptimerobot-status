073c0650edbddc8e58b3d57ff316349547462131
makuning <makun_ing@126.com>
2024-11-14 13:16:09 +0800

    完成头部组件的重构

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id='header'>
+    <div class='container'>
+      <h1 class='logo'>{{ config.SiteName }}</h1>
+      <div class='navi'>
+        <link-component v-for="(item, index) in config.Navi" :key="index" :to="item.url" :text="item.text" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts' setup>
+import config from '@/common/config'
+import LinkComponent from '@/components/LinkComponent.vue'
+</script>
