0afe1dcbf7ddff28802a93b7ccf29ff7b0db5136
makuning <makun_ing@126.com>
2024-11-14 16:59:38 +0800

    增加ElementPlus并设置自定导入插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({// ElementPlus自动导入
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({// ElementPlus自动导入
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
