56bdb7a7df1563b34a7ae2cb719623edc49bbb5e
makuning <makun_ing@126.com>
2024-11-14 17:49:12 +0800

    完成主页的拼接

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
 <template>
-  <h1>欢迎</h1>
+  <header-component />
+  <div class="container">
+    <div id="uptime">
+        <uptime-robot-component v-for="apiKey in config.ApiKeys" :key="apiKey" :api-key="apiKey" />
+    </div>
+    <div id="footer">
+      <p>
+        参照 <link-component to="https://github.com/yb/uptime-status" text="Uptime-Status" /> 更改
+      </p>
+      <p>
+        基于 <link-component to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率
+        5 分钟
+      </p>
+      <p>
+        &copy; 2024 <link-component to="https://github.com/insectmk/uptimerobot-status" text="UptimeRobot-Status" />, Version
+        {{ Package.version }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import UptimeRobotComponent from '@/components/UptimeRobotComponent.vue'
+import LinkComponent from '@/components/LinkComponent.vue'
+import config from '@/common/config'
+import Package from '../../package.json'
 </script>
