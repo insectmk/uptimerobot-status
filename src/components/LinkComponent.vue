0df55a16cbeb3dd518b0462e12ea8dd9ab9d469d
makuning <makun_ing@126.com>
2024-11-14 13:13:50 +0800

    完成链接组件的重构

---
+++ src/components/LinkComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <a :href="to" target='_blank'>
+    {{ text }}
+  </a>
+</template>
+
+<script lang='ts' setup>
+import { defineProps } from 'vue'
+
+defineProps([ // 定义组件参数
+  'to',
+  'text'
+])
+</script>
